a8efc2407b313847cb29e807d81bb7dca3b8a1a7
zizu-07 <165991925+zizu-07@users.noreply.github.com>
2025-03-18 10:14:21 +0000

    AsciiTextImage.cs

--- AsciiTextImage.cs
+++ AsciiTextImage.cs
@@ -4,16 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ChatBotCyberSecurityApp
+namespace ChatBotCyberSecurityApp // Defining a namespace for the application
 {
-
-
-    internal class AsciiTextImage
+    internal class AsciiTextImage // Declaring an internal class named AsciiTextImage
     {
-        public AsciiTextImage()
+        public AsciiTextImage() // Constructor for the AsciiTextImage class
         {
-           
-            string enter = "Tap enter to continue";
+            string enter = "Tap enter to continue"; // Message prompting the user to press enter
             string title = @"
             
           
@@ -23,14 +20,13 @@ namespace ChatBotCyberSecurityApp
 ██║░░██╗░░╚██╔╝░░██╔══██╗██╔══╝░░██╔══██╗  ░╚═══██╗██╔══██║██║██╔══╝░░██║░░░░░██║░░██║
 ╚█████╔╝░░░██║░░░██████╦╝███████╗██║░░██║  ██████╔╝██║░░██║██║███████╗███████╗██████╔╝
 ░╚════╝░░░░╚═╝░░░╚═════╝░╚══════╝╚═╝░░╚═╝  ╚═════╝░╚═╝░░╚═╝╚═╝╚══════╝╚══════╝╚═════╝░
-
 ";
 
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.Write(enter);
-            Console.ForegroundColor = ConsoleColor.DarkCyan;
-            Console.Write(title);
-                            Console.ReadKey();
+            Console.ForegroundColor = ConsoleColor.White; // Setting the console text color to white
+            Console.Write(enter); // Displaying the prompt message to the user
+            Console.ForegroundColor = ConsoleColor.DarkCyan; // Changing the console text color to dark cyan
+            Console.Write(title); // Displaying the ASCII art title
+            Console.ReadKey(); // Waiting for a key press from the user
         }
     }
 }
