35337723f4153bfa4389c2d053cf8d7eea666931
zizu-07 <165991925+zizu-07@users.noreply.github.com>
2025-03-18 10:13:27 +0000

    VoiceMessage.cs

--- VoiceMessage.cs
+++ VoiceMessage.cs
@@ -1,24 +1,20 @@
-﻿using System;
-using System.Media;
+using System; // Importing the System namespace for basic functionalities
+using System.Media; // Importing the System.Media namespace to handle sound playback
 
-
-namespace ChatBotCyberSecurityApp
+namespace ChatBotCyberSecurityApp // Defining a namespace for the application
 {
-   
-
-    internal class VoiceMessage
+    internal class VoiceMessage // Declaring an internal class named VoiceMessage
     {
-        public VoiceMessage()
+        public VoiceMessage() // Constructor for the VoiceMessage class
         {
-            PlayVoiceMessage("Greeting Voice Over.wav");
+            PlayVoiceMessage("Greeting Voice Over.wav"); // Calling the method to play a specific voice message
         }
 
-        public static void PlayVoiceMessage(string fileName)
+        public static void PlayVoiceMessage(string fileName) // Static method to play a voice message
         {
-            SoundPlayer player = new SoundPlayer();
-            player.SoundLocation = fileName;
-            player.Play();
+            SoundPlayer player = new SoundPlayer(); // Creating an instance of SoundPlayer to handle sound playback
+            player.SoundLocation = fileName; // Setting the location of the sound file to be played
+            player.Play(); // Playing the sound file
         }
     }
-
 }
